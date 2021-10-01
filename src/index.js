49f62869feb22eda0e747246fc8c605ea61e1259
marcosmerida <marcosmerida@gmail.com>
2021-10-01 10:13:58 -0600

    tasks are now editable

--- src/index.js
+++ src/index.js
@@ -6,52 +6,62 @@ import './style.css';
 import taskcompleted from './status';
 
 /* Variable section */
-const allTasks = [];
 const ul = document.getElementById('list');
-/* This function manipulates the DOM creating a new li element */
-function addElements(e) {
-  allTasks.push(e);
-  const liTag = document.createElement('li');
-  const Task = `<input type = 'checkbox' id = ${e.index}><p id = ${e.index}>${e.description}</p><button class='removeBook' id='buttonremove' value='${e.index}'><i class="fas fa-trash-alt"></i></button>`;
-  liTag.innerHTML = Task.trim();
-  ul.appendChild(liTag);
+let allTasks = [];
+
+/* This function pushes the new task into the allTasks array and saves it in localStorage */
+function createTask(e) {
+  /*   the data that is stored in allTasks variable is updated here with the localstorage */
+  allTasks = JSON.parse(localStorage.getItem('data'));
+  if (allTasks === null) {
+    allTasks = [];
+  }
+  let newTask = { description: '', completed: false, index: allTasks.length };
+  newTask.description = e;
+  /*   This procedure here verify if input text value contains nothing. */
+  if (newTask.description === '') {
+    document.getElementById('task').placeholder = 'this field cannot be blank';
+  } else {
+    allTasks.push(newTask);
+    updateTasks(allTasks);
+  }
 }
 
-/* This function contains the default tasks and lodas the data from the localstorage */
-function defaultTasks() {
-  let defaultTask1 = JSON.parse(localStorage.getItem('default1'));
-  let defaultTask2 = JSON.parse(localStorage.getItem('default2'));
-  if (defaultTask1 === null) {
-    defaultTask1 = {
-      description: 'wash the dishes',
-      completed: false,
-      index: 'default1',
-    };
-  }
-  if (defaultTask2 === null) {
-    defaultTask2 = {
-      description: 'Complete To Do list project',
-      completed: false,
-      index: 'default2',
-    };
-  }
-
-  addElements(defaultTask1);
-  addElements(defaultTask2);
-
-  if (defaultTask1.completed === true) {
-    document.getElementById('default1').checked = true;
-  } else {
-    document.getElementById('default1').checked = false;
-  }
-  if (defaultTask2.completed === true) {
-    document.getElementById('default2').checked = true;
-  } else {
-    document.getElementById('default2').checked = false;
-  }
-
-  taskcompleted('default1', defaultTask1, 'default1');
-  taskcompleted('default2', defaultTask2, 'default2');
+/* This function updates the localstorage with new Tasks */
+function updateTasks(e) {
+  localStorage.setItem('data', JSON.stringify(e));
 }
 
-defaultTasks();
+/* This function lodas the data from the localstorage */
+function printScreen() {
+  ul.innerHTML = '';
+  let taskArray = JSON.parse(localStorage.getItem('data'));
+  console.log(taskArray);
+  if (taskArray === null) {
+    taskArray = [];
+  }
+  if (taskArray.length > 0) {
+    taskArray.forEach((number) => {
+      const taskOne = `<input type = 'checkbox' id = ${number.index}><input type="text" id = "task" value= "${number.description}"></input></input><button class='removeBook' id='buttonremove' value='${number.index}'><i class="fas fa-trash-alt"></i></button>`;
+      const liTag = document.createElement('li');
+      liTag.setAttribute('id', number.index);
+      liTag.innerHTML = taskOne.trim();
+      ul.appendChild(liTag);
+    });
+  } else {
+    const noTaskText = '<li><p>No Tasks for today!</p></li>';
+    const liTag = document.createElement('li');
+    liTag.innerHTML = noTaskText.trim();
+    ul.appendChild(liTag);
+  }
+}
+
+/* This function calls the data from the input text in HTML */
+document.getElementById('button').addEventListener('click', () => {
+  let textTask = document.getElementById('task').value;
+  createTask(textTask);
+  printScreen();
+});
+
+/* Functions and eventListeners excecuted */
+printScreen();
